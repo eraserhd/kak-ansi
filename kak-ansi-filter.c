519648804770d37812895387a40f4d940eb9a9a1
Jason Felice <jason.m.felice@gmail.com>
2019-05-31 15:16:56 -0400

    Sets foreground color

--- kak-ansi-filter.c
+++ kak-ansi-filter.c
@@ -5,8 +5,66 @@
 #include <wchar.h>
 #include <wctype.h>
 
+#define DEFAULT 9
+
+static const wchar_t* COLORS[] = {
+    L"black",
+    L"red",
+    L"green",
+    L"yellow",
+    L"blue",
+    L"magenta",
+    L"cyan",
+    L"white",
+    L"-",
+    L"default",
+};
+
+int     line = 1;
+int     column = 1;
+int     last_line = 1;
+int     last_column = 0;
+int     foreground = DEFAULT;
+int     start_line = 1;
+int     start_column = 1;
 wchar_t escape_sequence[1024];
 int     escape_sequence_length = 0;
+
+void process_ansi_escape(wchar_t* seq)
+{
+    int prev_foreground = foreground;
+
+    for (wchar_t* p = seq; *p; p++)
+    {
+        if (!iswdigit(p[0]) && iswdigit(p[1]))
+        {
+            int code = -1;
+            swscanf(p + 1, L"%d", &code);
+            if (code == -1)
+                continue;
+
+            if (code >= 30 && code <= 39)
+            {
+                foreground = code % 10;
+            }
+        }
+    }
+
+    if (prev_foreground != foreground && prev_foreground != DEFAULT)
+    {
+        fwprintf(stderr, L" %d.%d,%d.%d|%ls", start_line, start_column, last_line, last_column, COLORS[prev_foreground]);
+        start_line = line;
+        start_column = column;
+    }
+}
+
+void process_escape_sequence(wchar_t* seq)
+{
+    if (wcslen(seq) < 2)
+        return;
+    if (seq[1] == L'[' && seq[wcslen(seq)-1] == 'm')
+        process_ansi_escape(seq);
+}
 
 void add_escape_char(wchar_t ch)
 {
@@ -36,11 +94,25 @@ bool handle_escape_char(wchar_t ch)
     if (escape_sequence_length > 1)
     {
         add_escape_char(ch);
-
+        process_escape_sequence(escape_sequence);
         escape_sequence_length = 0;
         return true;
     }
     return false;
+}
+
+void display_char(wchar_t ch)
+{
+    putwchar(ch);
+    last_line = line;
+    last_column = column;
+    if (ch == L'\n')
+    {
+        ++line;
+        column = 1;
+    }
+    else
+        ++column;
 }
 
 int main(int argc, char* argv[])
@@ -49,13 +121,16 @@ int main(int argc, char* argv[])
 
     setlocale(LC_ALL, "en_US.utf8");
 
+    fwprintf(stderr, L"set-option -add buffer ansi_color_ranges");
+
     while ((ch = fgetwc(stdin)) != WEOF)
     {
         if (handle_escape_char(ch))
             continue;
 
-        putwchar(ch);
+        display_char(ch);
     }
 
+    fwprintf(stderr, L"\n");
     exit(0);
 }
--- tests/run.sh
+++ tests/run.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
 compile() {
-    $CC -o kak-ansi-filter kak-ansi-filter.c
+    $CC -g -o kak-ansi-filter kak-ansi-filter.c
+    return $?
 }
 
 runTest() {
     local test="$1"
     printf ' %s ... ' "$1"
-    ./kak-ansi-filter <"$test/in" |od -c >"$test/run"
-    if ! diff -q "$test/run" "$test/out" >/dev/null; then
-        printf '\e[31mfailed\e[0m\n'
-        diff -u "$test/run" "$test/out"
-    else
+    ./kak-ansi-filter 2>"$test/run-eval" <"$test/in" |od -c >"$test/run-out"
+    local ok=true
+    if [ -f "$test/out" ] && ! diff -q "$test/run-out" "$test/out" >/dev/null 2>&1; then
+        printf '\e[31mfailed (out)\e[0m\n'
+        diff -u "$test/out" "$test/run-out"
+        ok=false
+    fi
+    if [ -f "$test/eval" ] && ! diff -q "$test/run-eval" "$test/eval" >/dev/null 2>&1; then
+        printf '\e[31mfailed (eval)\e[0m\n'
+        diff -u "$test/eval" "$test/run-eval"
+        ok=false
+    fi
+    if $ok; then
         printf 'ok\n'
     fi
 }
 
 main() {
-    compile
+    compile || return $?
     local test
     for test in tests/*; do
         if ! [ -d "$test" ]; then
@@ -27,4 +36,4 @@ main() {
     done
 }
 
-main "$@"
+main "$@" || return $?
