3ffef65075ea3556e16021934781da966f594c94
Jason Felice <jason.m.felice@gmail.com>
2019-05-31 23:42:28 -0400

    emits new faces for fg change

--- tests/functions.bash
+++ tests/functions.bash
@@ -5,14 +5,14 @@ h2() {
 t() {
     local in
     local has_out=false out
-    local has_range=false range
+    local ranges=()
     local description="$1"
     shift
     while (( $# > 0 )); do
         case "$1" in
             -in) shift; in="$1";;
             -out) shift; has_out=true; out="$1";;
-            -range) shift; has_range=true; range="$1";;
+            -range) shift; ranges+=( "$1" );;
         esac
         shift
     done
@@ -28,13 +28,18 @@ t() {
         printf '      Expected output: %s\n' "$out"
         printf '        Actual output: %s\n' "$actual_out"
         printf '\n'
-    elif $has_range && ! grep -qF " $range" "$commands"; then 
-        ok=false
-        printf '\e[31mfailed\e[0m\n'
-        printf '      Expected range: %s\n' "$range"
-        printf '       Commands were: %s\n' "$(cat "$commands")"
-        printf '\n'
     fi
+    for range in "${ranges[@]}"; do
+        if ! grep -qF " $range" "$commands"; then 
+            if $ok; then
+                printf '\e[31mfailed\e[0m\n'
+            fi
+            ok=false
+            printf '      Expected range: %s\n' "$range"
+            printf '       Commands were: %s\n' "$(cat "$commands")"
+            printf '\n'
+        fi
+    done
     rm -f "$commands"
     if $ok; then
         printf 'ok\n'
